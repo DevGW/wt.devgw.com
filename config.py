a30c345e594e5953ab1e8dd9821aaf634804459d
DevGW <jason@devgw.com>
2025-04-10 17:52:17 -0500

    db work x4

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ class Config:
     Application configuration settings for WeightTracker.
     """
     SECRET_KEY = os.environ.get('SECRET_KEY', 'your-default-secret-key')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://wtuser:Bl4hCr4p%21%40@127.0.0.1/weight_tracker')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://wtuser:Bl4hCr4p%21%40%21%40@127.0.0.1/weight_tracker')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = os.environ.get('DEBUG', False)
